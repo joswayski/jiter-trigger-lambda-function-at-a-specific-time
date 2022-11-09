2eceb3407fde29dd0618e052768da8a795dd817a
Jose Valerio <contact@josevalerio.com>
2022-11-08 22:40:45 -0500

    Saving

--- function.ts
+++ function.ts
@@ -1,3 +1,9 @@
-export const main = async (event) => {
+export const main = async (event: any) => {
   console.log(`Incoming Event:\n`, event);
+
+  const response = {
+    statusCode: 200,
+    body: "Hello world!",
+  };
+  return response;
 };
