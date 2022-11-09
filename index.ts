d65449352ff55407054eb7f9f96b2c37ff2561ca
Jose Valerio <contact@josevalerio.com>
2022-11-08 23:37:02 -0500

    Added index

---
+++ index.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import Jiter from "@jiter/node";
+import * as dayjs from "dayjs";
+
+Jiter.init({
+  apiKey: "YOUR_API_KEY",
+  signingSecret: "YOUR_SIGNING_SECRET",
+});
+
+const main = async () => {
+  try {
+    const payload = {
+      action: "Email.Welcome",
+      userId: "87",
+    };
+    await Jiter.Events.createEvent({
+      destination: "YOUR_LAMBDA_URL",
+      payload: JSON.stringify(payload),
+      scheduledTime: dayjs().add(10, "seconds").toISOString(),
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+main();
